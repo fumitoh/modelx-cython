e1bd2a90b4cf1cbdfc8b1c1efbb641e8684cd560
Fumito Hamamura <fumito.ham@gmail.com>
2023-10-19 22:27:03 +0900

    CHG: Update sample script

--- examples/run_basicterm_s.py
+++ examples/run_basicterm_s.py
@@ -3,6 +3,7 @@ import os
 import timeit
 import psutil
 
+is_nomx = '--nomx' in sys.argv
 is_cy = '--cython' in sys.argv
 
 process = psutil.Process(os.getpid())
@@ -10,17 +11,16 @@ def get_mem_use():
     """Return the memory usage of the current process in MB"""
     return process.memory_info().rss / (1024 ** 2)
 
-if not is_cy:
+if is_nomx:
     from BasicTerm_S_nomx import BasicTerm_S
-else:
+elif is_cy:
     from BasicTerm_S_nomx_cy import BasicTerm_S
+else:
+    raise RuntimeError("Parameter missiong")
 
 result = {}
 def run_model(m, size):
     mem_use = get_mem_use()
-    # for i in range(1, size+1):
-    #     for t in range(0, m.Projection[i].proj_len()):
-    #         m.Projection[i].mort_rate(t)
     result['value'] = sum(m.Projection[i].pv_net_cf() for i in range(1, size+1)) / size
     result['mem_use'] = get_mem_use() - mem_use
 
