e97c81cf2e2b5e9a1728fb2f7e9ede52c7f7a35a
Fumito Hamamura <fumito.ham@gmail.com>
2023-10-21 17:32:42 +0900

    CLN: Remove comments in sample.py

--- examples/sample.py
+++ examples/sample.py
@@ -2,8 +2,3 @@ from BasicTerm_S_nomx import BasicTerm_S
 
 for i in range(1, 11):
     BasicTerm_S.Projection[i].pv_net_cf()
-
-# from Mortgage_typed import mx_model
-
-# for t in range(3):
-#     mx_model.Fixed(Term=(t+1) * 10, Rate=0.03).Balance((t+1) * 10)
